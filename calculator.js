54eb4412385004141f010b4d1faef335a9a5ec94
ashwinikadam2005 <159120329+ashwinikadam2005@users.noreply.github.com>
2024-07-13 14:31:55 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+var expr=document.querySelector('#expression')
+var action=document.querySelectorAll('.calculator .buttons .actions button')
+
+action.forEach((button)=>{
+    button.addEventListener('click', function(){
+        let previousValue=expr.value
+        previousValue=previousValue+button.value
+        expr.value=previousValue
+    })
+})
+var numbers=document.querySelectorAll('.calculator .buttons .numbers .btn1')
+numbers.forEach((button)=>{
+    button.addEventListener('click', function(){
+        let previousValue=expr.value
+        previousValue=previousValue+button.value
+        expr.value=previousValue
+    })
+})
+function calculate(){
+    let expression=expr.value
+    let result=eval(expression)
+    expr.value=result.toString()
+}
+function clearText(){
+    expr.value=''
+}
